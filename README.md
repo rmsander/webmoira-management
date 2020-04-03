9d9210acdd1ca5d01f2dddcb34e3a1a3b021b005
rmsander <rmsander@mit.edu>
2020-04-03 10:17:34 -0400

    Added command line arguments to user-specific emails; updated readme

--- README.md
+++ README.md
@@ -5,7 +5,29 @@ A Python and bash-based helper tool for automating the addition and deletion of 
 Simply clone this repo, and make sure you have `ssh`, `scp`, and Python set up on your command line!  These scripts were run and developed on Ubuntu 18.04, but should work on any operating system.
 
 ## Use
-The primary use of this repository is to automatically add or delete many members from a MIT Web Moira email list at once (for adding/deleting only a couple of members at a time, check out the [WebMoira Tool](https://groups.mit.edu/webmoira/).  
+This tool features an automated WebMoira client for updating email lists, as well as a tool for sending automated emails with user-specific information (`email_user_information.py`).  Each of these features are discussed below.
+
+### Automating User Information Emails
+Have a spreadsheet with user-specific information you want to send out?  You've come to the right place.  The Python file `email_user_information.py` allows you to read a spreadsheet file with email addresses and user information columns, and send out emails to each user with their specific user information.
+   
+**NOTES**: 
+
+1. You'll need to edit the `body` variable in the function `mail_to_list` to create the body of the email.
+
+2. Make sure you enable less secure google apps for the specific gmail address you're using.  See the link [here](https://myaccount.google.com/lesssecureapps).
+ 
+3. You'll likely need to edit how user-specific information is read in from the spreadsheet (i.e. edit the `df` object in the main function).
+ 
+ Once ready, you can test your script with (the script will print the email information to the Python console):
+ 
+```python email_user_information.py <gmail> <gmail_password> <email_subject> <path/to/file.xlsx> --test```
+ 
+ And once you're satisfied with the quality of your emails, you can send them by simply removing the `--test` flag:
+  
+```python email_user_information.py <gmail> <gmail_password> <email_subject> <path/to/file.xlsx>```
+
+### Automating the WebMoira Tool
+One of the primary uses of this repository is to automatically add or delete many members from a MIT Web Moira email list at once (for adding/deleting only a couple of members at a time, check out the [WebMoira Tool](https://groups.mit.edu/webmoira/).  
 
 This codebase uses command-line arguments to determine what email management actions should be taken.  Commands can be selected by calling the Python file in a command line environment:
 
--- email_user_information.py
+++ email_user_information.py
@@ -5,31 +5,46 @@ import pandas as pd
 import argparse
 
 
-def mail_to_list(email_list, info_list, gmail, password, subject):
+def mail_to_list(email_list, info_list, gmail, password, subject, test_only=False):
     """Send an email to everyone in the provided csv."""
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
-    server.login(gmail, password)
+    print("GMAIL : {}, PASSWORD: {}".format(gmail, password))
+
+    #server.login(gmail, password)
 
     # Count how many emails have been sent
     i = 0
 
-    num_users = len(info)
+    num_users = len(info_list)
     for email, info in zip(email_list, info_list):
         body = """PUT TEXT OF YOUR EMAIL HERE AND USE .format(info) to insert
                   email-specific information into the email."""
-        msg = MIMEMultipart()
-        msg['From'] = gmail
-        msg['To'] = email
-        msg['Subject'] = subject
-        msg.attach(MIMEText(body, 'plain'))
-        text = msg.as_string()
-        server.sendmail(gmail, email, text)
-        print('Sent email %s out of %s' % ((i + 1), num_users))
-        i += 1
+
+        if test_only:  # If you want to do a test run first
+            print(
+                "_______________________________________________________________\n"
+                "TEST ONLY: {}".format(test_only))
+            print("TESTING: ROW {}".format(i))
+            print("Email body: {} \n GMAIL (sending from): {} \n"
+                  "Current email: {} \n Info: {} \n Subject: {}".format(
+                body, gmail, email, info, subject))
+            print("_______________________________________________________________")
+            i += 1
+
+        else:  # Once happy with formatting, send the emails
+            msg = MIMEMultipart()
+            msg['From'] = gmail
+            msg['To'] = email
+            msg['Subject'] = subject
+            msg.attach(MIMEText(body, 'plain'))
+            text = msg.as_string()
+            server.sendmail(gmail, email, text)
+            print('Sent email %s out of %s' % ((i + 1), num_users))
+            i += 1
 
     # Notify user that emails have finished sending
     print('Done sending emails.')
@@ -43,8 +58,8 @@ def parse_args():
                                       "@gmail.com", type=str)
     parser.add_argument("gmail_PW", help="Your gmail password.", type=str)
     parser.add_argument("subject", help="Email subject.", type=str)
-    parser.add_argument("-f", "--file", help="Read from Excel file",
-                        required=False, default=None)
+    parser.add_argument("file", help="Read from Excel file", type=str)
+    parser.add_argument("-t", "--test", required=False, action="store_true")
 
     # Parse arguments
     results = vars(parser.parse_args())
@@ -57,19 +72,20 @@ def parse_args():
     pw = results["gmail_PW"]
     subject = results["subject"]
     file = results["file"]
+    test = results["test"]
 
-    return gmail, pw, subject, file
+    return gmail, pw, subject, file, test
 
 
 def main():
     # Parse args and read in data, if we have
-    gmail, pw, subject, file = parse_args()
+    gmail, pw, subject, file, test = parse_args()
     df = pd.read_excel(file)  # Read as excel file
 
     # Default form: Spreadsheet headers are 'email' and 'info' (where info can be more than one column)
     email_list = df['email']  # Email column
     info_list = df['info']    # Info column(s)
-    mail_to_list(email_list, info_list, gmail, pw, subject)
+    mail_to_list(email_list, info_list, gmail, pw, subject, test_only=test)
 
 
 if __name__ == "__main__":
